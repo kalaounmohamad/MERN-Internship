980551b22c4952ba1fa4c515902933113e1b4bff
kalaounmohamad <kalaounmohamad@hotmail.com>
2023-10-01 05:58:10 +0300

    Established base of frontend

---
+++ frontend/src/components/Layout/Header.js
@@ -0,0 +1,46 @@
+//rafce
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <nav className="navbar row sticky-top">
+        <div className="col-12 col-md-3">
+          <img src="/images/logo.webp" alt="logo" className="logo" />
+        </div>
+        <div className="col-12 col-md-6 mt-2 mt-md-0">
+          <div className="input-group">
+            <input
+              type="text"
+              id="search_field"
+              className="form-control"
+              placeholder="Enter Your Favorite Restaurant:"
+            />
+            <div className="input-group-append">
+              <button className="btn" id="search_btn">
+                <i className="fa fa-search" aria-hidden="true"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* login */}
+        <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+          <button className="btn" id="login_btn">
+            Login
+          </button>
+          <span className="ml-3" id="cart">
+            Cart
+          </span>
+          <span className="ml-1" id="cart_count">
+            2
+          </span>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
+
+//rfce
