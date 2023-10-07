a578d0b8e2e7ad4f08d45aa1a320d2547d8c9723
kalaounmohamad <kalaounmohamad@hotmail.com>
2023-10-08 01:25:43 +0300

    Forget password and reset it using email

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -59,7 +59,8 @@ const Header = () => {
             <div className="ml-4 dropdown d-inline">
               <Link
                 to="/"
-                className="btn dropdown-toggle text-white-mr-4"
+                className="btn dropdown-toggle text-white mr-4"
+                type="button"
                 id="dropDownMenuButton"
                 data-toggle="dropdown"
                 aria-haspopup="true"
